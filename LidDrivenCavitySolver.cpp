f7f4790c473fecd750491a60731bbc89b9fbc960
Toby Freeland <trf16@spitfire.ae.ic.ac.uk>
2020-03-12 15:23:06 +0000

    Added cartesian grid structure. TODO: add check that n process = Ny * Nx

--- LidDrivenCavitySolver.cpp
+++ LidDrivenCavitySolver.cpp
@@ -7,12 +7,28 @@ using namespace std;
 int main(int argc, char* argv[])
 {   
 	MPI_Init(&argc, &argv); //Init MPI
+
+	//Get rank and overall size
 	int rank, size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	
 
-	cout << "Yo! I am a process and my rank is:" << rank << endl;
+	//Display a message from each process
+	cout << "Yo! I am a process and my rank is:" << rank <<"/" << size << endl;
+
+	MPI_Comm domain_grid; //Define a communicator for the grid
+	const int dims = 2; //Working on a 2D flow
+	int sizes[dims] = {4,4}; //Needs to be changed by the arguments
+	int periods[dims] = {0,0};
+	int reorder = 1;
+	int coords[dims];
+	int grid_rank;
+	MPI_Cart_create(MPI_COMM_WORLD, dims, sizes, periods, reorder, &domain_grid); //Create a cartesian grid
+	MPI_Comm_rank(domain_grid, &grid_rank);
+	MPI_Cart_coords(domain_grid, grid_rank, dims, coords);
+
+	cout << "I live at coords:" << coords << endl;
+
    	 // Create a new instance of the LidDrivenCavity class
     	LidDrivenCavity* solver = new LidDrivenCavity();
 
