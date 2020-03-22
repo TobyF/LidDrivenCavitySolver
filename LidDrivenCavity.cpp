c0c82b8eab5a238f087cf07faaf8dea20437b1e6
Toby Freeland <trf16@spitfire.ae.ic.ac.uk>
2020-03-22 03:45:43 +0000

    Added a lot of debug comments. Bug Fixed - no longer deadlocks.

--- LidDrivenCavity.cpp
+++ LidDrivenCavity.cpp
@@ -46,7 +46,7 @@ LidDrivenCavity::~LidDrivenCavity()
 
 void LidDrivenCavity::Test()
 {
-  cout << "(Rank" << rank << ") I have " << Nx << "x points and " << Ny << "y points" << " my neighbours are:" << neighbours[0] << neighbours[1] << neighbours[2] << neighbours[3] << endl;
+  //cout << "(Rank" << rank << ") I have " << Nx << "x points and " << Ny << "y points" << " my neighbours are:" << neighbours[0] << neighbours[1] << neighbours[2] << neighbours[3] << endl;
 
   //cout << "I am a process, rank: " << rank << " and my neighbours are:" << neighbours[0] << neighbours[1] << neighbours[2] << neighbours[3] << endl;
 }
@@ -165,31 +165,40 @@ void LidDrivenCavity::UpdateSharedInterfaces(){
         send_left[i] = v[Ny + i];
         send_right[i] = v[(Nx-2)*Ny + i];
       }
-
+      cout << rank << " send/recv data flowing left <---" << endl;
       MPI_Sendrecv(&send_left, Ny, MPI_DOUBLE, neighbours[0], 0,
-                &recv_right, Ny , MPI_DOUBLE, neighbours[1], 0,
+                &recv_right, Ny , MPI_DOUBLE, neighbours[1], MPI_ANY_TAG,
                 grid_comm,  MPI_STATUS_IGNORE);
 
+      cout << rank << " send/recv data flowing left --->" << endl;
       MPI_Sendrecv(&send_right, Ny, MPI_DOUBLE, neighbours[1], 1,
-                &recv_left, Ny , MPI_DOUBLE, neighbours[0], 1,
+                &recv_left, Ny , MPI_DOUBLE, neighbours[0], MPI_ANY_TAG,
                 grid_comm,  MPI_STATUS_IGNORE);
     }
     else if ((neighbours[0] != -2)){ //attached to right wall and left dree
+      cout << rank << " send data flowing left <---" << endl;
       MPI_Send(&send_left, Ny, MPI_DOUBLE, neighbours[0],0,grid_comm);
-      MPI_Recv(&recv_left, Ny, MPI_DOUBLE, neighbours[0],1,grid_comm, MPI_STATUS_IGNORE);
+      cout << rank << " recv data flowing right --->" << endl;
+      MPI_Recv(&recv_left, Ny, MPI_DOUBLE, neighbours[0],MPI_ANY_TAG,grid_comm, MPI_STATUS_IGNORE);
 
     }
     else{ //Attached to left wall
-      MPI_Recv(&recv_right, Ny, MPI_DOUBLE, neighbours[1],0,grid_comm, MPI_STATUS_IGNORE);
+      cout << rank << " recv data flowing left <---" << endl;
+      MPI_Recv(&recv_right, Ny, MPI_DOUBLE, neighbours[1],MPI_ANY_TAG,grid_comm, MPI_STATUS_IGNORE);
+      cout << rank << " send data flowing right --->" << endl;
       MPI_Send(&send_right, Ny, MPI_DOUBLE, neighbours[1],1,grid_comm);
     }
+
 
     //Unpack any recvievd files, updating the stored arrays
     for (i=0;i<Ny;i++){
         if (neighbours[0] != -2) { v[i] = recv_left[i];}
         if (neighbours[1] != -2) {v[(Nx-1)*Ny + i] = recv_right[i];}
     }
+    cout << rank << " Unpacked horizontal data" << endl;
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank == 0){cout << "----Vertical----" << endl;}
     //Vertical
     double send_up[Ny];
     double send_down[Ny];
@@ -201,23 +210,26 @@ void LidDrivenCavity::UpdateSharedInterfaces(){
         send_down[i] = v[Ny*i+1]; //Second from bottom row
         send_up[i] = v[Ny*(i+1)-2]; //Second to top row
       }
-
+      cout << rank << "Yep im here - 1" << endl;
       MPI_Sendrecv(&send_down, Nx, MPI_DOUBLE, neighbours[2], 2,
-                &recv_up, Nx , MPI_DOUBLE, neighbours[3], 2,
+                &recv_up, Nx , MPI_DOUBLE, neighbours[3], MPI_ANY_TAG,
                 grid_comm,  MPI_STATUS_IGNORE);
+      cout << rank << "Now im here - 2" << endl;
 
       MPI_Sendrecv(&send_up, Nx, MPI_DOUBLE, neighbours[3], 3,
-                &recv_down, Nx , MPI_DOUBLE, neighbours[2], 3,
+                &recv_down, Nx , MPI_DOUBLE, neighbours[2], MPI_ANY_TAG,
                 grid_comm,  MPI_STATUS_IGNORE);
+
+      cout << rank << "Now im finished" << endl;
     }
     else if ((neighbours[2] != -2)){ //attached to top lid
       MPI_Send(&send_down, Nx, MPI_DOUBLE, neighbours[2],2,grid_comm);
-      MPI_Recv(&recv_down, Nx, MPI_DOUBLE, neighbours[3],3,grid_comm, MPI_STATUS_IGNORE);
+      MPI_Recv(&recv_down, Nx, MPI_DOUBLE, neighbours[2],MPI_ANY_TAG,grid_comm, MPI_STATUS_IGNORE);
 
     }
     else{ //Attached to bottom wall
-      MPI_Recv(&recv_up, Nx, MPI_DOUBLE, neighbours[3],2,grid_comm, MPI_STATUS_IGNORE);
-      MPI_Send(&send_up, Nx, MPI_DOUBLE, neighbours[2],3,grid_comm);
+      MPI_Recv(&recv_up, Nx, MPI_DOUBLE, neighbours[3],MPI_ANY_TAG,grid_comm, MPI_STATUS_IGNORE);
+      MPI_Send(&send_up, Nx, MPI_DOUBLE, neighbours[3], 3,grid_comm);
     }
 
     //Unpack any recvievd files, updating the stored arrays
@@ -225,20 +237,28 @@ void LidDrivenCavity::UpdateSharedInterfaces(){
         if (neighbours[2] != -2) { v[Ny*i] = recv_down[i];}
         if (neighbours[3] != -2) {v[Ny*(i+1)-1] = recv_up[i];}
     }
+    cout << rank << " Unpacked Vertical Data" << endl;
+    //MPI_Barrier(grid_comm);
 
 }
 
 void LidDrivenCavity::Integrate()
 {
+  if (rank == 0){cout << "Integrating..." << endl;}
   //Sets the boundary values of vorticity based on current stream function (if attached to wall
   UpdateBoundaryConditions();
-
+  MPI_Barrier(grid_comm);
   // Sets the interior vorticity values.
+  if (rank == 0){cout << "Step 2: Calculating Interior Vorticity" << endl;}
   CalculateInteriorVorticity();
-
+  MPI_Barrier(grid_comm);
   // GET VORTICITY AT DOMAIN INTERFACES
+  if (rank == 0){cout << "Update the shared boundary points" << endl;}
   UpdateSharedInterfaces();
 
+  MPI_Barrier(grid_comm);
+
+  if (rank == 0){cout << "Step 3: Calculate Future Interior Vorticity" << endl;}
   CalculateFutureInteriorVorticity();
 
 
